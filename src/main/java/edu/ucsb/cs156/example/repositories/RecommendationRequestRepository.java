260c44ec89212fe81b96a36368f2304666be84e3
Daisy <wanqian@ucsb.edu>
2025-04-25 02:51:31 -0700

    created database table for RecommendationRequest

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
@@ -0,0 +1,15 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.RecommendationRequest;
+
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The UCSBDiningCommonsRepository is a repository for UCSBDiningCommons entities
+ */
+@Repository
+public interface RecommendationRequestRepository extends CrudRepository<RecommendationRequest, String> {
+ 
+}
